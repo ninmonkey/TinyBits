1b4afc23f27611dc1d3353472039563e4f6f9421
Jake Bolton <ninmonkeys@gmail.com>
2025-09-17 16:55:53 -0500

    new: ArgumentCompleters for RgbColors

---
+++ pwsh/color/pansies/ArgumentCompleter-For-RgbColor.ps1
@@ -0,0 +1,58 @@
+﻿Import-Module Pansies
+<#
+.SYNOPSIS
+    Add Color completions using Pansies
+.notes
+    Taken from: https://github.com/PoshCode/Pansies/blob/448454654f2b722932623564e48431831c9aaaab/Source/Private/_init.ps1#L73-L88
+.EXAMPLE
+
+    # Try partial names like:
+
+        > WriteColor -FgColor b<tab>
+#>
+
+# Enable extra colors:
+[PoshCode.Pansies.RgbColor]::ColorMode = 'Rgb24Bit'
+$Now = Get-Date
+
+function WriteColor {
+    <#
+    .SYNOPSIS
+        Complete and write both
+    #>
+   [cmdletBinding()]
+   param(
+        [PoshCode.Pansies.RgbColor] $FgColor = 'gray60',
+        [PoshCode.Pansies.RgbColor] $BgColor = 'gray30',
+
+        [Parameter( ValueFromPipeline )]
+        [Alias('Text')]
+        $InputObject,
+
+        [switch] $PassThrud
+    )
+    process {
+        if( $PassThru ) {
+            $InputObject | Pansies\New-Text -fg $FgColor -bg $BgColor
+            return
+        }
+        $InputObject
+            | Pansies\New-Text -fg $FgColor -bg $BgColor
+            | Join-String -f '    {0}'
+            | Pansies\Write-Host
+    }
+}
+
+
+Register-ArgumentCompleter -CommandName 'WriteColor' -ParameterName 'FgColor' -ScriptBlock $RgbColorCompleter
+Register-ArgumentCompleter -CommandName 'WriteColor' -ParameterName 'BgColor' -ScriptBlock $RgbColorCompleter
+
+$now | WriteColor black goldenrod
+$now | WriteColor -Fg gray20 -Bg gray50
+$now | WriteColor
+$now | WriteColor -Bg gray20 -Fg gray50
+$now = Get-Date
+
+$prevDt = $now
+$now    = Get-Date
+'Elapsed: {0:n2} ms' -f ($now - $prevDt).TotalMilliseconds | WriteColor
---
+++ pwsh/color/pansies/AutoBackgroundColor-WithContrast.ps1
@@ -0,0 +1,43 @@
+﻿Import-Module Pansies
+
+# required import
+$dotSrc = Get-Item -ea 'stop' ( Join-Path $PSScriptRoot './ArgumentCompleter-For-RgbColor.ps1' )
+. (Get-Item -ea 'stop' $DotSrc)
+
+function TestAutoContrast {
+    [CmdletBinding()]
+    param(
+        [int] $Count = 180,
+        [int] $HueStep = 10,
+        [switch] $HighContrast =  $true,
+        [switch] $BlackAndWhite = $false
+        # [int] $BrightStep
+        # [RgbColor] $Color,
+    )
+
+    pansies\Get-ColorWheel -Count $Count -HueStep $HueStep | Foreach-Object {
+        $splat_cmpl = @{
+            Color         = $_
+            HighContrast  = $HighContrast
+            BlackAndWhite = $BlackAndWhite
+        }
+        $autoBg = pansies\Get-Complement @splat_cmpl
+
+        $_.X11ColorName | WriteColor -PassThru  -fg $_ -bg $autoBg
+    } | Join-String -sep ' '
+}
+
+'Testing names: [ gray00, ..., gray100 ]' | Write-Host
+
+0..99 | ?{ $_ % 3 -eq 0 } | %{
+    $name = 'gray{0}' -f $_
+     [RgbColor] $cur = $name
+     $splat_cmpl = @{
+            Color         = $cur
+            HighContrast  = $true
+            BlackAndWhite = $false
+     }
+     $autoBg = pansies\Get-Complement @splat_cmpl
+     $cur.X11ColorName | WriteColor -PassThru  -fg $cur -bg $autoBg
+
+}| Join-String -sep ' '
