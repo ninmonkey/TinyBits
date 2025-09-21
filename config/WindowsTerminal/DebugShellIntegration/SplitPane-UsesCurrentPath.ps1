6facea130ec798cc8d8f9037ed931b6713a23d8e
Jake Bolton <ninmonkeys@gmail.com>
2025-09-20 19:58:05 -0500

    pwsh: wt: Test shell integration

---
+++ config/WindowsTerminal/DebugShellIntegration/SplitPane-UsesCurrentPath.ps1
@@ -0,0 +1,30 @@
+﻿<#
+.SYNOPSIS
+    Visualize the shell integration for CWD
+.description
+    implemented clone dir from: https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory
+
+#>
+
+# to disable both:
+$log1, $log2 = $false, $true
+
+# enable just the 2nd one:
+$log1, $log2 = $false, $true
+
+$OriginalPrompt = $function:Prompt
+'Saved $OriginalPrompt' | Write-Host -fg 'salmon'
+
+function Prompt { @(
+    "`n`n"
+    $loc = $executionContext.SessionState.Path.CurrentLocation
+    if($script:log1) {  $loc | Write-Host -fg 'red' }
+
+    [string] $str = ''
+    if ($loc.Provider.Name -eq "FileSystem") {
+            $str += "`e]9;9;`"$($loc.ProviderPath)`"`e\"
+    }
+    $str += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+    $str
+    if( $script:log2 ) {  $str -replace "`e", '␛' | Write-Host -fg 'green' -NoNewline }
+) | Join-String -sep "`n" }
