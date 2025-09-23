4af3f91cc3f92f547a83afd85f5e60f8a602a46b
Jake Bolton <ninmonkeys@gmail.com>
2025-09-22 22:33:23 -0500

    Add dynamic param types, export summary

--- pwsh/misc/Export-CommandDynamicParams.ps1
+++ pwsh/misc/Export-CommandDynamicParams.ps1
@@ -11,8 +11,10 @@ $tobj = $ExecutionContext.InvokeCommand.GetCommand(
     [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters )
 
 "`n`nUsing 'cert:\'" | Write-Host -fore blue
-pushd cert:\
+pushd cert:\ -StackName 'dynamicParams'
 ($tobj.Parameters.GetEnumerator() | ?{ $_.Value.IsDynamic }).Value | Ft
+
+popd -ea ignore -StackName 'dynamicParams'
 
 <#
     Name                    ParameterType
@@ -25,9 +27,10 @@ pushd cert:\
     ExpiringInDays          System.Int32
 #>
 "`n`nUsing 'C:\'" | Write-Host -fore blue
-
-pushd c:\
+pushd c:\ -StackName 'dynamicParams'
 ($tobj.Parameters.GetEnumerator() | ?{ $_.Value.IsDynamic }).Value | Ft
+
+popd -ea ignore -StackName 'dynamicParams'
 # or:  $tobj.Parameters.GetEnumerator() | ?{ $_.Value.IsDynamic }
 
 <#
@@ -43,11 +46,24 @@ function CollectRuntimeParams {
     param(
         [Parameter()]
         [ArgumentCompletions('Microsoft.PowerShell.Management\Get-Item')]
-        $Command = 'Microsoft.PowerShell.Management\Get-Item'
+        $Command = 'Microsoft.PowerShell.Management\Get-Item',
+
+        [System.Management.Automation.CommandTypes] $CommandType = [System.Management.Automation.CommandTypes]::Cmdlet
     )
 
+    # $targetCmd = $ExecutionContext.InvokeCommand.GetCommand(
+    #     # $Command, [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters )
+    #     $Command, [System.Management.Automation.CommandTypes]::Cmdlet, @{} )
     $targetCmd = $ExecutionContext.InvokeCommand.GetCommand(
-        $Command, [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters )
+        # $Command, [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters )
+        $Command, $CommandType, @{} )
+
+    if($null -eq $TargetCmd ) {
+        'Command: {0}, Type: {1} was null for GetCommand!' -f @(
+            $Command, $CommandType
+        ) | Write-Warning
+        return
+    }
 
     $dynamicParams = @( $targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic } )
     $nonDynamic    = @( $targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { -not $_.Value.IsDynamic } )
@@ -78,48 +94,43 @@ function CollectRuntimeParams {
     $Info = [ordered]@{
         DynamicParams = $Dynamicparams
         NormalParams  = $nonDynamic
-        TargetCommand = $targetCommand
+        TargetCommand = $targetCmd
         RuntimeParams = $runtimeParams
     }
     [pscustomobject]$Info
 }
 
+
+pushd 'c:\' -StackName 'dynamicParams'
+$from_fs = CollectRuntimeParams -Command Microsoft.PowerShell.Management\Get-Item
+
+pushd 'cert:\' -StackName 'dynamicParams'
+$from_cert = CollectRuntimeParams -Command Microsoft.PowerShell.Management\Get-Item
+
+popd -ea ignore -StackName 'dynamicParams'
+popd -ea ignore -StackName 'dynamicParams'
+
+'Cert Params and Attributes' | Write-Host -fore Blue
+
+$from_cert.RuntimeParams.Values | Ft
+
+@( $from_cert.RuntimeParams.Values).Attributes | % { $_.gettype() }
+
+'Filesystem Params and Attributes' | Write-Host -fore Blue
+
+$from_fs.RuntimeParams.Values | Ft
+
+@($from_fs.RuntimeParams.Values).Attributes | % { $_.gettype() }
+
+@'
+## Try commands: ##
+
+
+$from_fs.RuntimeParams.Values | Ft
+
+$from_cert.RuntimeParams.Values | Ft
+
+'@ | Write-Host -fore Blue
+
+
 return
-
-$finalD        = [ordered]@{}
-$dynamicParams = @()
-$nonDynamic    = @()
- try {
-        $targetCmd = $ExecutionContext.InvokeCommand.GetCommand(
-            'Microsoft.PowerShell.Management\Get-Item',
-            [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters )
-
-        $dynamicParams = @( $targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic } )
-        $nonDynamic = @( $targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { -not $_.Value.IsDynamic } )
-
-        if ($dynamicParams.Length -gt 0)
-        {
-            $paramDictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
-
-            [Management.Automation.RuntimeDefinedParameterDictionary] $pd_ctor = @{
-                Data = 'some data'
-                HelpFile = 'Help Str'
-            }
-
-            foreach ($param in $dynamicParams)
-            {
-                $param = $param.Value
-
-                if(-not $MyInvocation.MyCommand.Parameters.ContainsKey($param.Name))
-                {
-                    $dynParam = [Management.Automation.RuntimeDefinedParameter]::new($param.Name, $param.ParameterType, $param.Attributes)
-                    $paramDictionary.Add($param.Name, $dynParam)
-                }
-            }
-            $finalD = $paramDictionary
-
-            return $paramDictionary
-        }
-    } catch {
-        throw
-    }
