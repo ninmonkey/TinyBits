6befab1826af7aa939aaf6f2eac5805c59034efb
Jake Bolton <ninmonkeys@gmail.com>
2025-11-11 15:32:53 -0600

    add: pwsh: text `EncodeAs`, `DecodeAs`

--- pwsh/color/pansies/Export-PansiesEntities.ps1
+++ pwsh/color/pansies/Export-PansiesEntities.ps1
@@ -2,7 +2,7 @@
 #requires -Module Pansies
 <#
 .SYNOPSIS
-    Collect and shape a list of codepoints from Pansies Emoji list
+    A few examples of encoding and decoding text as unicode. And collect and shape a list of codepoints from Pansies Emoji list
 .LINK
     https://github.com/PoshCode/Pansies/blob/45ff80a8f6b4ae52a4d7c36cbeb91ca93b8ed3d1/Source/Assembly/Entities.Emoji.cs
 #>
@@ -31,43 +31,134 @@ function Export-PansiesEntities {
     $emojis = foreach( $key in [PoshCode.Pansies.Entities]::Emoji.Keys  ) {
         $text = [PoshCode.Pansies.Entities]::Emoji[ $key ]
         [pscustomobject]@{
-            Name = $key;
-            Text = $text
+            PSTypeName     = 'Pansies.Entity.Summary'
+            Name           = $key
+            Text           = $text
             CodepointsList = TextToCodepointsString $text
         }
     }
     return $emojis
 }
 
-function EncodeAsOther {
+function DecodeAs {
+    <#
+    .SYNOPSIS
+        Decodes string into bytes using specified encoding
+    #>
     param(
+        # Bytes to decode as some text
+        [byte[]] $Bytes,
+
+        # EncodingInfo Name
+        [ArgumentCompletions('utf-8', 'utf-16', 'utf-16le', 'ascii' )]
+        $EncodingName = 'utf-8'
+    )
+    try { $enc = [Text.Encoding]::GetEncoding( $EncodingName ) }
+    catch { throw }
+
+    $Text = $enc.GetString( $Bytes )
+
+    [pscustomobject]@{
+        PSTypeName    = 'TextDecoding.Summary'
+        EncodingName  = $Enc.WebName
+        Text          = $Text
+        Bytes         = $Bytes
+        NumBytes      = $Bytes.Count
+        TextLen       = $Text.Length
+        TextRuneCount = @( $Text.EnumerateRunes() ).Count
+    }
+}
+function EncodeAs {
+    <#
+    .SYNOPSIS
+        Converts string into bytes using specified encoding
+    #>
+    param(
+        # Text to encode
         [string] $Text,
+
+        # EncodingInfo Name
+        [ArgumentCompletions('utf-8', 'utf-16', 'utf-16le', 'ascii' )]
         $EncodingName = 'utf-8'
     )
 
-    $Enc = [Text.Encoding]::GetEncoding( $EncodingName )
-
+    # $Enc = [Text.Encoding]::GetEncoding( $EncodingName )
     # ensure exception on errors
-    try {
-        [Text.Encoding]::GetEncoding( $EncodingName )
-    } catch { throw }
+    try { $enc = [Text.Encoding]::GetEncoding( $EncodingName ) }
+    catch { throw }
 
     [byte[]] $Bytes = @( $Enc.GetBytes( $Text ) )
 
     [pscustomobject]@{
-        InputText = $Text
-        Encoding  = $Enc.WebName
-        Bytes = $Bytes
+        PSTypeName    = 'TextEncoding.Summary'
+        EncodingName  = $Enc.WebName
+        Text          = $Text
+        Bytes         = $Bytes
+        NumBytes      = $Bytes.Count
+        TextLen       = $Text.Length
+        TextRuneCount = @( $Text.EnumerateRunes() ).Count
     }
-
-    # if( $Enc.WebName -eq 'windows-1252' ) { throw "Failed to GetEncoding('$EncodingName') ! Windows-1252 was returned instead!" }
 }
+
+function Convert-Text-ToInlineStringExpression {
+    <#
+    .EXAMPLE
+        > Convert-PansiesEntity-ToInlineExpression -Text ([PoshCode.Pansies.Entities]::Emoji['zombie'])
+            # optionally
+            # | Join-String -Double | Set-Clipboard
+
+        # output:
+            "$( [char]::ConvertFromUtf32( 0x1f9df ) )"
+    #>
+    [Alias('Convert-PansiesEntity-ToInlineExpression')]
+    param(
+        [string] $Text
+    )
+
+$template = @'
+$( [char]::ConvertFromUtf32( 0x{0} ) )
+'@
+
+    ( $Text.EnumerateRunes() ).Value.ForEach({
+        $template -f $_.ToString('x')
+    })
+}
+    # orig: -f ( [PoshCode.Pansies.Entities]::Emoji['zombie'].EnumerateRunes()  ).Value.ToString('x') | cl
 
 
 $list = Export-PansiesEntities
+'## found some' | Write-Host -fg 'tomato'
 
-$list | ? name -match 'zz'
-
+$list | ? name -match 'zz' | ft -auto
 # if pwsh
-"`u{01f18e}"
-([Text.Rune] 0x001f18e).ToString()
+<#
+    "`u{01f18e}"
+    ([Text.Rune] 0x001f18e).ToString()
+#>
+
+$src = "`u{01f18e}"
+$out = EncodeAs -Text $src -EncodingName 'utf-8'
+
+'show correct, and incorrect decodings' | Write-Host -fg 'tomato'
+@(
+    DecodeAs -Bytes $bytes.Bytes -EncodingName 'utf-8'
+    DecodeAs -Bytes $bytes.Bytes -EncodingName 'utf-16'
+    DecodeAs -Bytes $bytes.Bytes -EncodingName 'utf-16le'
+    DecodeAs -Bytes $bytes.Bytes -EncodingName 'ascii'
+) | Ft -auto
+
+# $out = EncodeAs -Text $src -EncodingName 'utf-8'
+# DecodeAs -Bytes $Out.Bytes -EncodingName 'utf-8'
+
+'## convert entities from runes to inline expressions' | Write-Host -fg 'tomato'
+[string] $inline_exp = @( foreach ( $cur in $tryStr ) {
+    Convert-Text-ToInlineStringExpression -Text $cur
+})  | Join-String -sep '' | Join-String -DoubleQuote
+
+$inline_exp
+
+@'
+## try:
+
+    $bytes.Bytes, $src, $out, $inline_exp, $list,
+'@ | Write-host -fg 'salmon'
