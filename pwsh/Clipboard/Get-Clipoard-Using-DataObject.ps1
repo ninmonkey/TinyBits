846aabfe38c33e682c89c45efd00d2386ed6deaf
Jake Bolton <ninmonkeys@gmail.com>
2025-11-22 23:30:56 -0600

    new: pwsh: `Get-ClipboardDataFormat`s

--- pwsh/Clipboard/Get-Clipoard-Using-DataObject.ps1
+++ pwsh/Clipboard/Get-Clipoard-Using-DataObject.ps1
@@ -4,8 +4,18 @@ Add-Type -AssemblyName System.Windows.Forms
 
 <#
 .LINK
-https://github.com/dotnet/winforms/blob/62ebdb4b0d5cc7e163b8dc9331dc196e576bf162/src/System.Windows.Forms/src/System/Windows/Forms/OLE/DataObject.cs
+    https://learn.microsoft.com/en-us/windows/win32/dataxchg/html-clipboard-format
+.LINK
+    https://github.com/dotnet/winforms/blob/62ebdb4b0d5cc7e163b8dc9331dc196e576bf162/src/System.Windows.Forms/src/System/Windows/Forms/OLE/DataObject.cs
 .NOTES
+
+HTML uses clipboard type 'CF_HTML'
+
+See types:
+
+    - [Windows.Forms.Clipboard]
+    - [Windows.Forms.DataObject]
+    - [Windows.Forms.TextDataFormat]
 #>
 
 <#
@@ -34,15 +44,36 @@ Get methods:
 
 # Any Data present?
 function Test-ClipboardDataPresent {
+    <#
+    .SYNOPSIS
+        Query which formats are stored in the clipboard
+    .link
+        TinyBits\Test-ClipboardDataPresent
+    .link
+        TinyBits\Get-ClipboardDataFormat
+    #>
     param(
-        [DataObject] $DataObject
+        [DataObject] $DataObject,
+
+        [switch] $FromClipboard,
+
+        [Alias('AsString')]
+        [switch] $NameOnly
     )
 
     $format_names = [DataFormats]
         | Find-Member -MemberType Field
         | % Name | Sort-Object -Unique
 
+    if( -not $PSBoundParameters.ContainsKey('DataObject') ) {
+        $DataObject = [Clipboard]::GetDataObject()
+    }
+
     foreach( $name in $format_names ) {
+        if( $NameOnly -and $DataObject.GetDataPresent( $name ) ) {
+            $name
+            continue
+        }
         [pscustomObject]@{
             DataFormats    = $name
             GetDataPresent = $DataObject.GetDataPresent( $name )
@@ -50,6 +81,60 @@ function Test-ClipboardDataPresent {
     }
 }
 
+function Get-ClipboardDataFormat {
+    <#
+    .SYNOPSIS
+        Get data from clipboard, in many formats
+    .notes
+    original:
+        Content = $DataObject.GetData( $found_types[1].DataFormats )
+    .link
+        TinyBits\Test-ClipboardDataPresent
+    .link
+        TinyBits\Get-ClipboardDataFormat
+    .LINK
+        https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.dataobject?view=windowsdesktop-9.0
+    #>
+    param(
+        [Parameter(Position = 0)]
+        [DataObject] $DataObject,
+
+        [Parameter( ValueFromPipeline, ValueFromPipelineByPropertyName )]
+        [Alias('Name', 'DataFormats')]
+        [ArgumentCompletions(
+            'Bitmap', 'CommaSeparatedValue', 'Dib', 'Dif', 'EnhancedMetafile', 'FileDrop', 'Html', 'Locale', 'MetafilePict', 'OemText', 'Palette', 'PenData', 'Riff', 'Rtf', 'Serializable', 'StringFormat', 'SymbolicLink', 'Text', 'Tiff', 'UnicodeText', 'WaveAudio'
+        )]
+        [string[]] $FormatName
+        # [System.Windows.Forms.DataFormats[]] $FormatName
+    )
+
+    if( -not $PSBoundParameters.ContainsKey('DataObject') ) {
+        $DataObject = [Clipboard]::GetDataObject()
+    }
+
+    foreach( $name in $FormatName ) {
+        [pscustomobject]@{
+            DataFormats = $name
+            Content = $DataObject.GetData( $name )
+        }
+    }
+
+}
+
 ( $test_types = Test-ClipboardDataPresent -DataObject $Do )
 $found_types = $test_types | Where-Object GetDataPresent
 $found_types | Join-String -p DataFormats -f ' - {0}' -sep "`n" -op "Data Formats Present in Clipboard DataObject:`n"
+# original
+# $do.GetData( $found_types[1].DataFormats )
+
+$query = Get-ClipboardDataFormat -DataObject $do -FormatName $found_types.DataFormats
+$query.count
+
+
+return
+# Get-ClipboardDataFormat -DataObject $c_do -FormatName
+
+
+$c_do = [Clipboard]::GetDataObject()
+Get-ClipboardDataFormat -DataObject $c_do -FormatName ( [string[]] (
+    Test-ClipboardDataPresent -DataObject $c_do | ? GetDataPresent | % DataFormats))
