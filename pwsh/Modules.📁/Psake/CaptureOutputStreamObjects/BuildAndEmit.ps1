9fd0215a2698e8b7c3b765c4b9aa28aa7f464414
Jake Bolton <ninmonkeys@gmail.com>
2025-11-24 20:12:31 -0600

    example: new: call `Invoke-PSake`

---
+++ pwsh/Modules.📁/Psake/CaptureOutputStreamObjects/BuildAndEmit.ps1
@@ -0,0 +1,27 @@
+﻿Task default -Depends TaskA, TaskB, TaskC
+
+Properties {
+    $x = 512
+}
+
+FormatTaskName {
+    param($taskName)
+    Write-Host "Executing Task: $taskName" -ForegroundColor blue
+}
+
+Task TaskA {
+    'TaskA is executing'
+}
+
+Task TaskB {
+    'TaskB is executing'
+}
+
+Task TaskC {
+    'TaskB is executing'
+    [PSCustomObject]@{
+        PSTypeName = 'psake.info'
+        Name       = $Name
+        Num        = $x
+    }
+}
---
+++ pwsh/Modules.📁/Psake/CaptureOutputStreamObjects/InvokeTask-BuildAndEmit-2.ps1
@@ -0,0 +1,8 @@
+﻿$target = Get-Item './BuildAndEmit-2.ps1'
+
+$return_all = Invoke-psake $target -parameters @{ name = 'Jel' } -properties @{ Last = 'doe' }
+$final = $return_all | ?{ $_ -isnot [string] }
+$final | Ft -AutoSize
+
+'Try: $final, or for the full list, see: $return_all' | Write-Host -fg 'orange'
+# Invoke-psake $target -parameters @{ name = 'Jel'; x = 33 } -properties @{ Last = 'doe'; x = 30 }
