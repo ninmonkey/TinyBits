5f684685bae4f0d8cfd04450ae88babf9edcda80
Jake Bolton <ninmonkeys@gmail.com>
2025-11-29 21:44:09 -0600

    example: added `Encoding.Convert( <enc1>, <enc2>, <bytes> )`

--- pwsh/TextEncoding/ConvertFrom-EncToEnc-Overload.ps1
+++ pwsh/TextEncoding/ConvertFrom-EncToEnc-Overload.ps1
@@ -48,3 +48,55 @@ H1 '[2] $Example => Encode Utf8 => Decode Ascii'
 $bytes = [Text.Encoding]::Utf8.GetBytes( $exampleText )
 [Text.Encoding]::Ascii.GetString( $bytes )
     # causes bad output: 'Foo????Bar'
+
+
+$enc1250 = [Text.Encoding]::GetEncoding(1250)
+$enc1251 = [Text.Encoding]::GetEncoding(1251)
+$enc8    = [Text.Encoding]::UTF8
+$enc16   = [Text.Encoding]::Unicode                      # is: utf-16 LE
+$enc16BE = [Text.Encoding]::BigEndianUnicode             # is: utf-16 BE
+$encCyr  = [Text.Encoding]::GetEncoding('windows-1251')
+
+h1 '[3] GetEncoding() for 1250, 1251'
+$enc1250.GetBytes( $exampleText ) | ToByteCsv
+$enc1251.GetBytes( $exampleText ) | ToByteCsv
+
+h1 '[3] $Example => Encode: UTF8 => Decode: 1250'
+$enc1250.GetString( $enc8.GetBytes( $ExampleText ) )
+
+h1 '[3] $Example => Encode: UTF8 => Decode: 1250'
+$enc1251.GetString( $enc8.GetBytes( $ExampleText ) )
+
+h1 '[3] $Example => Encode: UTF-16 => Decode: UTF8'
+$enc8.GetString( $enc16.GetBytes( $exampleText ) )
+# out: Foo=��Bar
+
+h1 '[3] $Example => Encode: UTF-16 BE => Decode: UTF8'
+$enc8.GetString( $enc16Be.GetBytes( $exampleText ) )
+# out: Foo�=�Bar
+
+h1 '[3] $Example => Encode: UTF-16 BE => Decode: Cyrillic (Win)'
+$encCyr.GetString( $enc16.GetBytes( $exampleText ))
+
+h1 '[3] $Example => Encode: UTF-8 => Decode: Cyrillic (Win)'
+$encCyr.GetString( $enc8.GetBytes( $exampleText ))
+
+# ... more
+$enc_oem437      = [Text.Encoding]::GetEncoding( 437 )  # aka: 'IBM437'
+$enc_oemCyrillic = [Text.Encoding]::GetEncoding( 855 )  # aka: 'IBM855'
+
+h1 '[3] $Example => Encode: UTF-8 => Decode: "OEM United States"'
+$enc_oem437.GetString( $enc8.GetBytes( $exampleText ))
+
+h1 '[3] $Example => Encode: UTF-8 => Decode: "OEM Cyrillic"'
+$enc_oemCyrillic.GetString( $enc8.GetBytes( $exampleText ))
+
+h1 '[4] using [Text.Encoding]::Convert( <enc1>, <enc2>, <bytes> )'
+
+$orig_bytes = $enc8.GetBytes( $exampleText )
+$new_bytes  = [Text.Encoding]::Convert( $enc8, $enc16, $orig_bytes )
+$enc16.GetString( $new_bytes )
+
+h1 '[3] Search encodings by name: -match "cy" or "oem"'
+[Text.Encoding]::GetEncodings()
+    | ? DisplayName  -Match 'cy|oem'
